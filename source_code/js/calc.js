146a76cc68f02c57598d6e049e5fc75658334093
WahyuRohmanto <wahyurohmanto113@gmail.com>
2022-09-19 18:02:23 +0700

    Pertemuan 5

---
+++ source_code/js/calc.js
@@ -0,0 +1,49 @@
+function hitung(value) {
+  var frm = document.getElementById("calcForm");
+  var a1 = parseFloat(frm.angka1.value);
+  var a2 = parseFloat(frm.angka2.value);
+  var oke = value;
+
+  switch (oke) {
+    case "btn_tambah":
+      if (isNaN(a1) || isNaN(a2)) {
+        alert("Harap masukkan angka!!");
+      } else {
+        var total = a1 + a2; //rumus
+        frm.hasil.value = total; //penempatan hasil
+      }
+      break;
+    case "btn_kurang":
+      if (isNaN(a1) || isNaN(a2)) {
+        alert("Harap masukkan angka!!");
+      } else {
+        var total = a1 - a2; //rumus
+        frm.hasil.value = total; //penempatan hasil
+      }
+      break;
+    case "btn_kali":
+      if (isNaN(a1) || isNaN(a2)) {
+        alert("Harap masukkan angka!!");
+      } else {
+        var total = a1 * a2; //rumus
+        frm.hasil.value = total; //penempatan hasil
+      }
+      break;
+    case "btn_bagi":
+      if (isNaN(a1) || isNaN(a2)) {
+        alert("Harap masukkan angka!!");
+      } else {
+        var total = a1 / a2; //rumus
+        frm.hasil.value = total; //penempatan hasil
+      }
+      break;
+    case "btn_pangkat":
+      if (isNaN(a1) || isNaN(a2)) {
+        alert("Harap masukkan angka!!");
+      } else {
+       var total =  Math.pow(a1, a2);
+        frm.hasil.value = total; //penempatan hasil
+      }
+      break;
+  }
+}
